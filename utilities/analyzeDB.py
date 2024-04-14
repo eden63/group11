fc78a9c2c2b2d44c3c344fecbf06725860971cf7
eden63 <eden.livni25@gmail.com>
2024-04-14 10:23:26 +0300

    requirments

--- utilities/analyzeDB.py
+++ utilities/analyzeDB.py
@@ -4,19 +4,22 @@ from utilities.db_manager import *
 ## print all documents in the collection
 
 # print all documents in the users collection
-print("Users collection:")
-for doc in DB.users.find():
-    print(doc)
-print()
+def print_users():
+    print("Users collection:")
+    for doc in DB.users.find():
+        print(doc)
+
 
 # print all documents in the payments collection
-print("Payments collection:")
-for doc in DB.payments.find():
-    print(doc)
-print()
+def print_payments():
+    print("Payments collection:")
+    for doc in DB.payments.find():
+        print(doc)
+
 
 # print all documents in the classes collection
-print("Classes collection:")
-for doc in DB.classes.find():
-    print(doc)
-print()
+def print_classes():
+    print("Classes collection:")
+    for doc in DB.classes.find():
+        print(doc)
+
